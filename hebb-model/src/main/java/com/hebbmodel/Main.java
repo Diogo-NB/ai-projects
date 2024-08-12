6cef631e7c65f12ff25c582063785afaab116278
Diogo-NB <diogonunes.20022@gmail.com>
2024-08-12 00:10:23 -0300

    Separate training and testing actions

--- hebb-model/src/main/java/com/hebbmodel/Main.java
+++ hebb-model/src/main/java/com/hebbmodel/Main.java
@@ -25,23 +25,33 @@ public class Main extends JFrame {
 
         mainPanel.add(gridsPanel, BorderLayout.CENTER);
 
-        // Create the button with text
         JButton trainButton = new JButton("Train model & Continue");
-        trainButton.setHorizontalTextPosition(SwingConstants.LEFT);
-        trainButton.setIcon(UIManager.getIcon("ComboBox.buttonArrowIcon"));
-
         trainButton.addActionListener(e -> {
-
             // Gets the data representations of both grids
             int[][] grid1Data = grid1.getGridData();
             int[][] grid2Data = grid2.getGridData();
 
-            JFrame testingFrame = new TestingFrame(grid1Data, grid2Data);
+            // Creates an object that represents the Hebbian model
+            HebbModel model = new HebbModel(100);
+
+            int[][] inputs = new int[2][100]; // training input arrays
+
+            // Converts the matrix representations into one-dimensional arrays
+            inputs[0] = Util.flattenMatrix(grid1Data);
+            inputs[1] = Util.flattenMatrix(grid2Data);
+
+            int[] expectedOutputs = new int[] { 1, -1 }; // expected output values for each input
+
+            // Trains the model with the input vectors and expected outputs
+            model.train(inputs, expectedOutputs);
+
+            // Open the frame for testing the model
+            JFrame testingFrame = new TestingFrame(model);
             testingFrame.setVisible(true);
         });
 
         JPanel buttonPanel = new JPanel();
-        buttonPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0)); // Add padding above the buttons
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
         buttonPanel.add(trainButton);
 
         mainPanel.add(buttonPanel, BorderLayout.SOUTH);
--- hebb-model/src/main/java/com/hebbmodel/TestingFrame.java
+++ hebb-model/src/main/java/com/hebbmodel/TestingFrame.java
@@ -5,9 +5,15 @@ import javax.swing.*;
 
 public class TestingFrame extends JFrame {
 
-    public TestingFrame(int[][] grid1Data, int[][] grid2Data) {
+    private HebbModel model;
+    private PixelGrid testGrid;
+    private JPanel resultsPanel;
+    private int resultIndex = 0;
+
+    public TestingFrame(HebbModel model) {
+        this.model = model;
         setTitle("Test model");
-        setSize(600, 600);
+        setSize(620, 600);
         setResizable(false);
 
         JPanel mainPanel = new JPanel(new BorderLayout(10, 10));
@@ -15,66 +21,47 @@ public class TestingFrame extends JFrame {
 
         JPanel gridsPanel = new JPanel();
         gridsPanel.setLayout(new GridLayout(1, 2, 10, 0));
-
-        PixelGrid testGrid = new PixelGrid("Test grid");
+        testGrid = new PixelGrid("Test grid");
         gridsPanel.add(testGrid);
 
-        mainPanel.add(gridsPanel, BorderLayout.CENTER);
-
-        JPanel buttonPanel = new JPanel();
-        buttonPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0)); // Add padding above the buttons
-
-        JPanel resultsPanel = new JPanel();
+        resultsPanel = new JPanel();
         resultsPanel.setLayout(new BoxLayout(resultsPanel, BoxLayout.Y_AXIS));
         resultsPanel.setBorder(BorderFactory.createCompoundBorder(
                 BorderFactory.createLineBorder(Color.BLACK, 1, true),
                 BorderFactory.createEmptyBorder(10, 10, 10, 10)));
-
-        resultsPanel.setPreferredSize(new Dimension(100, 300));
-        resultsPanel.setMinimumSize(new Dimension(100, 150));
-
-        JButton testButton = new JButton("Test");
-        testButton.addActionListener(e -> {
-            // Creates an object that represents the Hebbian model
-            HebbModel model = new HebbModel(100);
-
-            /* ------------------------- Training process --------------------------- */
-
-            int[][] inputs = new int[2][100]; // training input arrays
-
-            // Converts the matrix representations into one-dimensional arrays
-            inputs[0] = Util.flattenMatrix(grid1Data);
-            inputs[1] = Util.flattenMatrix(grid2Data);
-
-            int[] expectedOutputs = new int[] { 1, -1 }; // expected output values for each input
-
-            // Trains the model with the input vectors and expected outputs
-            model.train(inputs, expectedOutputs);
-
-            /* ---------------------------- Testing ------------------------------ */
-
-            int[][] testGridData = testGrid.getGridData();
-            int[] testInput = Util.flattenMatrix(testGridData);
-
-            // Add the results to the results panel
-            float result = model.test(testInput);
-            if (result > 0) {
-                resultsPanel.add(new JLabel("Grid 1 (" + result + ")"));
-            } else {
-                resultsPanel.add(new JLabel("Grid 2 (" + result + ")"));
-            }
-
-            resultsPanel.revalidate();
-            resultsPanel.repaint();
-        });
-
-        buttonPanel.add(testButton);
+        resultsPanel.setPreferredSize(new Dimension(120, 300));
+        resultsPanel.setMinimumSize(new Dimension(120, 150));
         resultsPanel.add(new JLabel("Results"));
 
+        JButton testButton = new JButton("Test model");
+        testButton.addActionListener(e -> {
+            testModelAction();
+        });
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
+        buttonPanel.add(testButton);
+
+        mainPanel.add(gridsPanel, BorderLayout.CENTER);
         mainPanel.add(buttonPanel, BorderLayout.SOUTH);
         mainPanel.add(resultsPanel, BorderLayout.EAST);
 
         add(mainPanel);
     }
 
+    public void testModelAction() {
+        int[][] testGridData = testGrid.getGridData();
+        int[] testInput = Util.flattenMatrix(testGridData);
+
+        // Add the results to the results panel
+        float result = model.test(testInput);
+        if (result > 0) {
+            resultsPanel.add(new JLabel("#" + resultIndex++ + " Grid 1 (" + result + ")"));
+        } else {
+            resultsPanel.add(new JLabel("#" + resultIndex++ + " Grid 2 (" + result + ")"));
+        }
+
+        resultsPanel.revalidate();
+        resultsPanel.repaint();
+    }
 }
