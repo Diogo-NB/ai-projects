72c41685b73240cd9c6dec465bc0bae65063dc6c
Diogo-NB <diogonunes.20022@gmail.com>
2024-09-01 18:13:40 -0300

    Add Vector class

--- madeline-network/src/main/java/com/ann/util/RandomUtil.java
+++
@@ -1,9 +0,0 @@
-package com.ann.util;
-
-public class RandomUtil {
-
-    public static float randomFloat(float min, float max) {
-        return (float) (Math.random() * (max - min) + min);
-    }
-    
-}
---
+++ madeline-network/src/main/java/com/ann/util/Vector.java
@@ -0,0 +1,159 @@
+package com.ann.util;
+
+// Vector class implementation for basic linear algebra operations
+// adapted from  https://medium.com/swlh/programming-linear-algebra-in-java-vector-operations-6ba08fdd5a1a
+public class Vector {
+
+    private float[] v;
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("(");
+        for (int i = 0; i < v.length; i++) {
+            sb.append(v[i]);
+            if (i < v.length - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append(")");
+        return sb.toString();
+    }
+
+    public Vector(float[] v) {
+        this.v = v;
+    }
+
+    public Vector(int[] v) {
+        this.v = new float[v.length];
+        for (int i = 0; i < v.length; i++) {
+            this.v[i] = v[i];
+        }
+    }
+
+    public Vector(Vector v) {
+        this.v = new float[v.v.length];
+        for (int i = 0; i < v.v.length; i++) {
+            this.v[i] = v.v[i];
+        }
+    }
+
+    public static Vector zeros(int n) {
+        return new Vector(new float[n]);
+    }
+
+    public static Vector random(int n) {
+        float[] v = new float[n];
+        for (int i = 0; i < n; i++) {
+            v[i] = (float) Math.random();
+        }
+        return new Vector(v);
+    }
+
+    public static Vector random(int n, float min, float max) {
+        float[] v = new float[n];
+        for (int i = 0; i < n; i++) {
+            v[i] = (float) (Math.random() * (max - min) + min);
+        }
+        return new Vector(v);
+    }
+
+    public static Vector flatten(Vector[] vectors) {
+        checkLengths(vectors);
+        int n = vectors.length * vectors[0].v.length;
+        Vector result = new Vector(new float[n]);
+        int k = 0;
+        for (int i = 0; i < vectors.length; i++) {
+            for (int j = 0; j < vectors[i].v.length; j++) {
+                result.v[k++] = vectors[i].v[j];
+            }
+        }
+        return result;
+    }
+
+    public float get(int i) {
+        return v[i];
+    }
+
+    public void set(int i, float f) {
+        v[i] = f;
+    }
+
+    public int size() {
+        return v.length;
+    }
+
+    public void setV(float[] v) {
+        this.v = v;
+    }
+
+    public float[] getV() {
+        return v.clone();
+    }
+
+    public void add(Vector v) {
+        add(this, v, this);
+    }
+
+    public static Vector add(Vector a, Vector b) {
+        Vector result = new Vector(a);
+        add(a, b, result);
+        return result;
+    }
+
+    public static void add(Vector a, Vector b, Vector result) {
+        checkLengths(a, b, result);
+        for (int i = 0; i < a.v.length; i++) {
+            result.v[i] = a.v[i] + b.v[i];
+        }
+    }
+
+    public void multiply(Vector v) {
+        multiply(this, v, this);
+    }
+
+    public static Vector multiply(Vector a, Vector b) {
+        Vector result = new Vector(a);
+        multiply(a, b, result);
+        return result;
+    }
+
+    public static void multiply(Vector a, Vector b, Vector result) {
+        checkLengths(a, b, result);
+        for (int i = 0; i < a.v.length; i++) {
+            result.v[i] = a.v[i] * b.v[i];
+        }
+    }
+
+    public void multiply(float scalar) {
+        multiply(this, scalar, this);
+    }
+
+    public static Vector multiply(Vector a, float scalar) {
+        Vector result = new Vector(a);
+        multiply(a, scalar, result);
+        return result;
+    }
+
+    public static void multiply(Vector a, float scalar, Vector result) {
+        checkLengths(a, result);
+        for (int i = 0; i < a.v.length; i++) {
+            result.v[i] = a.v[i] * scalar;
+        }
+    }
+
+    public static void checkLengths(Vector... vectors) {
+        if (vectors.length < 2) {
+            throw new IllegalArgumentException("At least two vectors are required.");
+        }
+
+        int firstSize = vectors[0].size();
+
+        for (int i = 1; i < vectors.length; i++) {
+            if (vectors[i].size() != firstSize) {
+                throw new IllegalArgumentException("Vectors are of different lengths");
+            }
+        }
+    }
+
+}
