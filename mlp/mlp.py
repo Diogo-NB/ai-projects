bb01b3b0dc872af16d60dfbe56757ddda68ebff2
Diogo-NB <diogonunes.20022@gmail.com>
2024-10-12 14:19:34 -0300

    feat: add predict method

--- mlp/mlp.py
+++ mlp/mlp.py
@@ -13,7 +13,7 @@ class MLP:
             self.b = np.random.uniform(lower_bound, upper_bound, n_outputs)      
     
     def __init__(self, layers_sizes: list[int], learning_rate: float = 0.01, tolerated_error: float = 0.005, max_epochs: int = 100000):
-        self.alpha = learning_rate
+        self.learning_rate = learning_rate
         self.tolerated_error = tolerated_error
         self.max_epochs = max_epochs
         self.n_layers = len(layers_sizes)
@@ -27,8 +27,9 @@ class MLP:
         self.layers_connections = layers_connections
 
     def train(self, inputs: ndarray, targets: ndarray):
-        alpha, max_epochs, tolerated_error, layers_connections = self.alpha, self.max_epochs, self.tolerated_error, self.layers_connections
+        learning_rate, max_epochs, tolerated_error, layers_connections = self.learning_rate, self.max_epochs, self.tolerated_error, self.layers_connections
         epochs = 0
+        alpha = learning_rate
         total_error = float('inf')
         x = inputs
         t = targets
@@ -51,7 +52,7 @@ class MLP:
 
             total_error += 0.5 * np.sum(dif ** 2)
 
-            delta = dif * (1 - y) * (1 + y)
+            g = dif * (1 - y) * (1 + y)
 
             # backpropagation
             for i in range(len(layers_connections) - 1, -1, -1):
@@ -59,15 +60,28 @@ class MLP:
                 z = z_list[i]
 
                 if i > 0:
-                    new_delta = np.dot(delta, layer_conn.w.T) * (1 - z) * (1 + z)
+                    new_g = np.dot(g, layer_conn.w.T) * (1 - z) * (1 + z)
 
-                delta_w = alpha * np.dot(z.T, delta) 
-                delta_b = alpha * np.sum(delta, axis=0)
-                layer_conn.w += delta_w
-                layer_conn.b += delta_b
+                g_w = alpha * np.dot(z.T, g) 
+                g_b = alpha * np.sum(g, axis=0)
+                layer_conn.w += g_w
+                layer_conn.b += g_b
 
-                delta = new_delta
-
-            if epochs % 1000 == 0: print(f"{epochs=} , {total_error=}")
+                g = new_g
+            
+            if epochs % 1000 == 0: print(f"{epochs=} , {total_error=}, {alpha=}")
 
         return total_error, epochs
+    
+    def predict(self, inputs: ndarray):
+        x = inputs
+        layers_connections = self.layers_connections
+
+        z = x
+        for layer_conn in layers_connections:
+            w = layer_conn.w
+            b = layer_conn.b
+            zin = np.dot(z, w) + b
+            z = np.tanh(zin)
+
+        return z
--- mlp/mlp_test.py
+++ mlp/mlp_test.py
@@ -2,11 +2,8 @@ import numpy as np
 from mlp import MLP
 import matplotlib.pyplot as plt
 
-x = np.linspace(-1, 1, 100)
+x = np.linspace(0, 1, 10)
 t = np.sin(np.pi * x)
-
-plt.plot(x, t)
-plt.show()
 
 x = x.reshape(-1, 1)
 t = t.reshape(-1, 1)
@@ -14,10 +11,15 @@ t = t.reshape(-1, 1)
 input_size = x.shape[1]
 output_size = t.shape[1]
 
-hidden_layers = [50, 50, 50, 50]
+hidden_layers = [3]
 
-mlp = MLP([input_size, *hidden_layers, output_size], learning_rate=0.01)   
+mlp = MLP(layers_sizes=[input_size, *hidden_layers, output_size], learning_rate=0.01)   
 
 error, epochs = mlp.train(x, t)
-
 print(f"Error: {error}, epochs: {epochs}")
+
+y = mlp.predict(x)
+
+plt.plot(x, t)
+plt.plot(x, y)
+plt.show()
